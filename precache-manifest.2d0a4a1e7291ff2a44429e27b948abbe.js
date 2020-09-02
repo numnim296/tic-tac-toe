6a3e8f0ee134de03730d03173a8ce2792ddbfbc6
muthita munmueang <muthita.mu.61@ubu.ac.th>
2020-09-02 10:37:52 +0700

    Updates

---
+++ precache-manifest.2d0a4a1e7291ff2a44429e27b948abbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797aa81d0cdd87cc955f53c85bf3116d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "c5b8e1359fd4ef93d182",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b8e1359fd4ef93d182",
+    "url": "/tic-tac-toe/static/js/main.90259f8e.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
